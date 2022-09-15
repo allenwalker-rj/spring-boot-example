c6ff7c09c7e8dda861db4212c5b535e06380ebae
allenwalker-rj <734403439@qq.com>
2022-09-15 16:47:13 +0800

    init springboot properties

---
+++ springboot-properties/src/main/java/com/allen/properties/SpringbootPropertiesApplication.java
@@ -0,0 +1,17 @@
+package com.allen.properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author allen
+ * @date 2022/9/15 16:44
+ */
+@SpringBootApplication
+public class SpringbootPropertiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPropertiesApplication.class, args);
+    }
+
+}
---
+++ springboot-properties/src/test/java/com/allen/properties/SpringbootPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allen.properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPropertiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
